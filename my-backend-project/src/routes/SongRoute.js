b7b94fc09fab810c09d35c895748cc823fc7046d
Chivu171 <tranchivu171@gmail.com>
2026-01-10 15:58:16 +0700

    delete all

--- my-backend-project/src/routes/SongRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const songController = require('../controllers/SongController');
-
-
-// GET /api/songs
-router.get('/getsongs', songController.getAllSongs);
-
-module.exports = router;
--- my-backend-project/src/routes/StreamRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const streamController = require('../controllers/StreamController');
-
-// GET /api/stream/song/:id
-router.get('/song/:id', streamController.streamSong);
-
-module.exports = router;
