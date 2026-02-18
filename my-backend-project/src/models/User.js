fcb66b2123031f4e0f5948246fc54a9f13472318
Chivu171 <tranchivu171@gmail.com>
2026-02-18 13:29:18 +0700

    feat: Add `fullName`, `avatar`, `role`, and `likedSongs` fields to the User schema, and enforce uniqueness for `username` and `email`.

--- my-backend-project/src/models/User.js
+++ my-backend-project/src/models/User.js
@@ -1,10 +1,17 @@
-const mongoose = require ('mongoose')
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username:{type:String, required:true},
-    email:{type:String,required: true},
-    password:{type:String, required:true}
-}, {timestamps :true}) // timestamps tự động thêm createdAt và updatedAt
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    fullName: { type: String, default: "" }, // Tên hiển thị
+    avatar: { type: String, default: "https://link-anh-mac-dinh.png" },
+    role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    likedSongs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Song'
+    }]
+}, { timestamps: true }) // timestamps tự động thêm createdAt và updatedAt
 
 const User = mongoose.model('User', userSchema)
-module.exports=  User
+module.exports = User
